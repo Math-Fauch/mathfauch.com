16dcfa8311528d357b01ac1132f1d79e00abe8b5
Math-Fauch <faucher.mathieu@gmail.com>
2025-07-24 18:50:40 -0400

    Changed project mdx and Made a presentation letter

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,9 +10,9 @@ const redis = Redis.fromEnv();
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const featured = allProjects.find((project) => project.slug === "portfolio")!;
+  const top2 = allProjects.find((project) => project.slug === "beneva")!;
+  const top3 = allProjects.find((project) => project.slug === "integration")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
