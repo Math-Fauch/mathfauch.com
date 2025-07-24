4d5bc5e5d1beb081efb41a6ca8eb50d480217b4c
Math-Fauch <faucher.mathieu@gmail.com>
2025-07-24 19:53:30 -0400

    Change term project

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -32,7 +32,7 @@ export const Navigation: React.FC = () => {
 							href="/projects"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							Projects
+							Expériences
 						</Link>
 						<Link
 							href="/contact"
